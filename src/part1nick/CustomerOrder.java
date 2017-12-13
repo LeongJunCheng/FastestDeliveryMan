b25056f76f83498d07320552c18145a62bb4c17b
Nickson <Nickson@Nickson>
2017-12-13 23:15:14 +0800

    Finished[#152831518]

--- src/part1nick/CustomerOrder.java
+++ src/part1nick/CustomerOrder.java
@@ -35,8 +35,8 @@ public class CustomerOrder {
             System.out.println("========================================================");
             System.out.println("1. Make Order");
             System.out.println("2. View Order Details");
-            System.out.println("3. Edit Order ");
-            System.out.println("4. Make payment");
+            System.out.println("3. Make payment");
+            System.out.println("4. ");
             System.out.println("5. Exit");
             System.out.println("========================================================");
 
@@ -46,37 +46,38 @@ public class CustomerOrder {
 
             if (selection == 1) {
                 boolean exit = false;
-                do{
-                System.out.println("========================================================");
-                System.out.println("No\tFood Name \t\t\t Price");
-                System.out.println("========================================================");
-                for (int u = 0; u < foodName.length; u++) {
-                    String[] FD = foodName[u].split(" - ");
-                    double price = Double.parseDouble(FD[1]);
+                do {
+                    System.out.println("========================================================");
+                    System.out.println("No\tFood Name \t\t\t Price");
+                    System.out.println("========================================================");
+                    for (int u = 0; u < foodName.length; u++) {
+                        String[] FD = foodName[u].split(" - ");
+                        double price = Double.parseDouble(FD[1]);
 //                        COD.CustomerOrderDomain(foodName[u]);
 //                        foodDetails.add(COD);
 
-                    System.out.printf("%d.\t%-10s\t\t\t RM %.2f \n", u + 1, FD[0], price);
-                }
-                System.out.printf("Select number(0 to exit): ");
-                foodSelect = s.nextInt();
-                if(foodSelect == 0)
-                {
-                    exit= true;
-                }else
-                //fooodOrder.add(foodName[foodSelect - 1]);
+                        System.out.printf("%d.\t%-10s\t\t\t RM %.2f \n", u + 1, FD[0], price);
+                    }
+                    System.out.printf("Select number(0 to exit): ");
+                    foodSelect = s.nextInt();
+                    if (foodSelect == 0) {
+                        exit = true;
+                    } else //fooodOrder.add(foodName[foodSelect - 1]);
+                    //COD.CustomerOrderDomain(foodName[foodSelect - 1]);
+                    //foodOrder.add(COD);
+                    {
+                        list.add(foodName[foodSelect - 1]);
 
-                //COD.CustomerOrderDomain(foodName[foodSelect - 1]);
-                //foodOrder.add(COD);
-                list.add(foodName[foodSelect - 1]);
-                }while (exit == false);
+                    }
+                } while (exit == false);
 
             } else if (selection == 2) {
-                System.out.flush();
+                double totaltemp = 0;
                 System.out.println("========================================================");
                 System.out.println("Food Name \t\t\t Price");
                 System.out.println("========================================================");
                 for (int i = 0; i < list.size(); i++) {
+
                     String tmp = (String) list.get(i);
                     String[] tmp2 = tmp.split(" - ");
                     String name = tmp2[0];
@@ -85,11 +86,13 @@ public class CustomerOrder {
                     //System.out.printf("%s\t\tRM %.2f \n", foodOrder.get(i).getFoodName(), foodOrder.get(i).getPrice());
                     System.out.printf("%-10s\t\t\tRM %04.2f \n", name, price);
                     //System.out.println(list.get(i));e();
-                    totalPrice += price;
+                    totaltemp += price;
+                    totalPrice = totaltemp;
                     int d = 0;
                     //totalPrice += foodOrder.get(i).getPric
 
                 }
+
                 System.out.println("========================================================");
                 System.out.printf("Total Price : RM %.2f \n", totalPrice);
                 s = new Scanner(System.in);
@@ -97,14 +100,91 @@ public class CustomerOrder {
                 String pressEnter = s.nextLine();
 
             } else if (selection == 3) {
-                System.out.println("3. Make payment");
+                int scPaymentType = 0;
+                System.out.println("Select Your Payment Method (0 to exit)");
+                System.out.println("========================================================");
+                System.out.println("1. Credit Card Payement");
+                System.out.println("2. Cash on delivery");
+                System.out.print("Selection : ");
+                scPaymentType = s.nextInt();
+
+                if (scPaymentType == 1) {
+
+                    boolean exit = false;
+
+                    do {
+                        String visaCardNum = "388576018410707";
+
+                        String enterVisaCardNum;
+                        System.out.println("Please enter card number: (16 digit)");
+                        enterVisaCardNum = s.next();
+
+                        if (enterVisaCardNum.length() == visaCardNum.length()) {
+                            
+                            if (enterVisaCardNum.equals(visaCardNum)) {
+                                
+                                String expiredDate = "20/20";
+                                String enterexpiredDate;
+                                System.out.println("Please enter expire Date and Year (Date/Year)");
+                                enterexpiredDate = s.next();
+                                
+                                if (enterexpiredDate.equals(expiredDate)) {
+                                    
+                                    String securityCode = "724";
+                                    String enterSecurityCode;
+                                    System.out.println("Please enter security code CVT");
+                                    enterSecurityCode = s.next();
+                                    
+                                    if (enterSecurityCode.equals(securityCode)) {
+                                        
+                                        char yesno;
+                                        System.out.printf("Your total price is RM %.2f \n", totalPrice);
+                                        System.out.println("Confirm Payment?(Y/N) ");
+                                        yesno = s.next().charAt(0);
+                                        
+                                        if (yesno == 'Y' || yesno == 'y') {
+                                            
+                                            System.out.println("Payment Sucessful.Your foods will be deliver soon!  ");
+                                            list.clear();
+                                            totalPrice = 0;
+                                            exit = true;
+
+                                        }
+                                    } else 
+                                        System.out.println("Wrong security code!");
+                                    
+                                } else 
+                                    System.out.println("Invalid expire date!");
+                                
+                            } else 
+                                System.out.println("Wrong card Number!");
+                            
+                        } else 
+                            System.out.println("Visa card number must be exact 16 digits!!");
+                        
+                    } while (exit == false);
+
+                } else if (scPaymentType == 2) {
+                    char yesno;
+                    System.out.print("Confirm Using cash payment?(extra 5% total fees will be charge)");
+                    System.out.printf("Your total price is RM %.2f after 5% charge RM%.2f\n", totalPrice, (totalPrice*1.5));
+                    
+                    yesno = s.next().charAt(0);
+                    if (yesno == 'Y' || yesno == 'y') {
+                        System.out.println("Payment Sucessful.Your foods will be deliver soon!  ");
+                        list.clear();
+                        totalPrice = 0;
+
+                    }
+                }
+
             } else if (selection == 5) {
                 System.out.println("Thank you for using the system.");
             } else {
                 System.out.println("Error selection.");
             }
 
-        } while (selection != 4);
+        } while (selection != 5);
 
     }
 
